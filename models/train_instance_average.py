335deffbba8349b855acdf5c3de8a6179f69cf3d
Yongxing Deng <yongxingd@gmail.com>
2013-10-29 22:32:29 -0700

    train instance average

--- models/train_instance_average.py
+++ models/train_instance_average.py
@@ -5,6 +5,7 @@
 """
 def train(profiles, convos):
     result = {}
+    global_convos = []
     for convo in convos:
         user1, user2 = str(convo["user1"]), str(convo["user2"])
         length = convo["lines1"] if convo["lines1"] else 0 + convo["lines2"] if convo["lines2"] else 0
@@ -12,7 +13,10 @@ def train(profiles, convos):
             if not user in result:
                 result[user] = []
             result[user].append(length)
-    return dict((user, float(sum(convos)) / len(convos)) for (user, convos) in result.iteritems())
+        global_convos.append(length)
+    return dict(\
+        users = dict((user, float(sum(convos)) / len(convos)) for (user, convos) in result.iteritems())
+        global_average = float(sum(global_convos)) / len(global_average))
 
 """
 Supports different feature-vector lengths for men and women.
@@ -23,7 +27,12 @@ Supports different feature-vector lengths for men and women.
 """
 def predict(profiles, convos, thetas):
     result = []
-    for male, female in convos:
+    users, global_average = thetas["users"], thetas["global_average"]
+    for user1, user2 in convos:
+        total = 0
+        for user in user1, user2:
+            if user in users:
+
         user1, user2 = str(male), str(female)
         if user1 in thetas and user2 in thetas:
             prediction = (thetas[user1] + thetas[user2]) / 2.0
