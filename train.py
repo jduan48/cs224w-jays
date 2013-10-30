bd42d562b7bbb2fefa240602de154973f0ae0e59
Yongxing Deng <yongxingd@gmail.com>
2013-10-29 23:53:57 -0700

    added a way to switch between models easily.

--- train.py
+++ train.py
@@ -2,13 +2,24 @@
 
 import csv, json, sys
 from random import random
-from models.train_instance_average import train, predict
-
+from sys import argv
 
 PROFILE_FILE = "input-data/json_profile_data"
 EDGE_TRAIN_FILE = "input-data/json_convo_data_train"
 EDGE_TEST_FILE = "input-data/json_convo_data_test"
 THETA_FILE = "tmp/theta"
+DEFAULT_FILE = "DEFAULT_MODEL"
+
+def importModule():
+    global model
+    if len(argv) == 1:
+        with open(DEFAULT_FILE, "r") as f:
+            model_name = f.read().strip()
+    else:
+        model_name = argv[1]
+        if model_name.endswith(".py"):
+          model_name = model_name[:-len(".py")]
+    model = __import__(model_name)
 
 def run():
     print "------------------ TRAINING -----------------"
@@ -16,9 +27,10 @@ def run():
         profiles = [json.loads(line) for line in f.readlines()]
     with open(EDGE_TRAIN_FILE, "r") as f:
         edges = [json.loads(line) for line in f.readlines()]
-    theta = train(profiles, edges)
+    theta = model.train(profiles, edges)
     with open(THETA_FILE, "w") as f:
         f.write(json.dumps(theta))
 
 if __name__ == "__main__":
+    importModule()
     run()
